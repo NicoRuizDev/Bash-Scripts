3e80686f4a0508d322e23388d9cb29db936fa128
Nico Ruiz <lifegamer2050@gmail.com>
2023-02-27 14:42:59 +0530

    Update ptero-backup.sh

--- ptero-backup.sh
+++ ptero-backup.sh
@@ -23,7 +23,6 @@ fi
 echo -e "${GREEN}Installing required packages...${NC}"
 apt update -y && apt upgrade -y
 apt install npm -y
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
 apt install nodejs -y
 apt-get install -y nginx git python3-certbot-nginx certbot
 npm install -g pm2
